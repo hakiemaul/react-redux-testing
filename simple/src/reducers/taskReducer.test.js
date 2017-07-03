91f76416ea69edb5765c0764bd424e548e38e77d
hakiemaul <noersedya@gmail.com>
2017-07-03 17:57:08 +0700

    reducer red phase testing

---
+++ simple/src/reducers/taskReducer.test.js
@@ -0,0 +1,42 @@
+import taskReducer from './taskReducer'
+
+describe('taskReducer testing', () => {
+  it('should return initial tasks state', () => {
+    const initialState = []
+
+    const expectedState = initialState
+    const actualState = taskReducer(initialState, null)
+
+    expect(actualState).toEqual(expectedState)
+  })
+
+  it('should add new task to state', () => {
+    const initialState = []
+    const newTask = {
+      id: 1,
+      tasks: 'Bla'
+    }
+    const action = {
+      type: 'ADD_TASK',
+      payload: newTask
+    }
+
+    const expectedState = [...initialState, newTask]
+    const actualState = taskReducer(initialState, action)
+
+    expect(actualState).toEqual(expectedState)
+  })
+
+  it('should remove task from state', () => {
+    const initialState = [{ id: 1, tasks: 'Bla' }]
+    const action = {
+      type: 'DELETE_TASK',
+      payload: 1
+    }
+
+    const expectedState = []
+    const actualState = taskReducer(initialState, action)
+
+    expect(actualState).toEqual(expectedState)
+  })
+})
